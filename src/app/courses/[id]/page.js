49acabc36b134e61ead6c3f629e476459d8dbd09
Malik Huzafah Khan <huzafahrajput10@gmail.com>
2024-06-07 17:49:35 +0500

    Added course details page

--- src/app/courses/[id]/page.js
+++ src/app/courses/[id]/page.js
@@ -3,44 +3,40 @@
 
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useRouter } from "next/navigation";
 import { Container, Heading, List, ListItem, Text } from "@chakra-ui/react";
 
-const CourseDetails = () => {
+const CourseDetails = ({ params }) => {
   const [course, setCourse] = useState(null);
-  const router = useRouter();
-  const { id } = router.query;
+  const { id } = params;
+
+  console.log(id);
+
+  const fetchCourse = async () => {
+    try {
+      const res = await axios.get(`http://localhost:5000/api/courses/${id}`);
+      console.log(res);
+      setCourse(res.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   useEffect(() => {
+    console.log(id);
     if (id) {
-      const fetchCourse = async () => {
-        try {
-          const res = await axios.get(
-            `http://localhost:5000/api/courses/${id}`
-          );
-          setCourse(res.data);
-        } catch (err) {
-          console.error(err);
-        }
-      };
-
       fetchCourse();
     }
   }, [id]);
 
-  if (!course) {
-    return <Text>Loading...</Text>;
-  }
-
   return (
     <Container maxW="container.md">
-      <Heading mb={6}>{course.title}</Heading>
-      <Text mb={4}>{course.description}</Text>
+      <Heading mb={6}>{course?.title}</Heading>
+      <Text mb={4}>{course?.description}</Text>
       <Heading size="md" mb={4}>
         Modules
       </Heading>
       <List spacing={3}>
-        {course.modules.map((module, index) => (
+        {course?.modules.map((module, index) => (
           <ListItem key={index}>
             <Text fontWeight="bold">{module.title}</Text>
             <List spacing={2}>
